ca4db2d205f425642e22d8af87b68428ef6ad42f
Clinton Wolfe <clinton@omniti.com>
2013-07-11 17:46:38 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# nad cookbook
+
+Install and configure nad, a nodejs-based monitoring agent.
+
+More info on nad: https://github.com/circonus-labs/nad
+
+## Platforms
+
+centos, ubuntu
+
+Other un*xes likely; windows unlikely
+
+## Recipes
+
+## LWRPs
+
+## Attributes
+
+
+
---
+++ metadata.rb
@@ -0,0 +1,7 @@
+maintainer       "OmniTI"
+maintainer_email "sa@omniti.com"
+license          "Modified BSD"
+description      "nad (nodejs monitoring agent)"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.1"
+name             "nad"
