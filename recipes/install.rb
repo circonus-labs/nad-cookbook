98c965f6e8fbe5ca33116e5540e1a939a36068ae
Clinton Wolfe <clinton@omniti.com>
2013-07-11 19:19:23 +0000

    solid ubuntu install from omnibus; still need release/arch switching for ubuntu

--- recipes/install.rb
+++ recipes/install.rb
@@ -2,44 +2,35 @@
 
 unless node[:nad][:enabled] then return end
 
-package "nodejs for nad" do
-  case node[:platform]
-  when 'ubuntu'
-    package_name 'nodejs'
-  when 'centos'
-    package_name 'npm' # EPEL
-  end
-end  
-
 file_name = node[:nad][:download_file]
 unless file_name then
   case node[:platform]
   when 'ubuntu'
     # TODO make this arch and release safe
-    local_file = 'ubuntu-12.04.1-64-nad.tar.gz'
+    file_name = 'ubuntu-12.04.1-64-nad.tar.gz'
   else
     #raise "TODO"
   end
 end
 
-#local_path = File.join(node[:nad][:tmp_path], file_name)
-#url = node[:nad][:download_url] + '/' + local_file
+local_path = File.join(node[:nad][:tmp_path], file_name)
+url = node[:nad][:download_url] + '/' + file_name
 
-#remote_file local_path do
-#   source nadPkgSource 
-#   mode 00644
-#   retries 5
-#   retry_delay 5
-#   only_if { ! File.exist ... } # TODO check for existing nad install
-#end
+remote_file local_path do
+  source url
+  mode 00644
+  retries 5
+  retry_delay 5
+  not_if { File.exist?('/opt/circonus/sbin/nad') }
+end
 
-# bash "unpack nad tarball" do
-#    cwd node[:nad][:tmp_path]
-#    command "tar xzf #{file_name} -C / && rm #{local_path}"
-#    only_if "test -f #{local_path}"
-# end
+bash "unpack nad tarball" do
+  code "tar xzf #{local_path} -C / && rm #{local_path}"
+  only_if "test -f #{local_path}"
+end
 
-# service "nad" do
-#    action [:enable, :start]
-# end
+service "nad (starter)" do
+  service_name 'nad'
+  action [:enable, :start]
+end
 
