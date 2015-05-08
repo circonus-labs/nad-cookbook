0c16852b77f268848122f134bd2eb7bd708eae8a
Philip Yuengling <pyuengling@client-10-8-3-83.office.omniti.com>
2015-05-08 15:36:37 -0400

    metadata tweaks

--- metadata.rb
+++ metadata.rb
@@ -4,10 +4,11 @@ license          "Modified BSD"
 description      "nad (nodejs monitoring agent)"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1.0"
-name             "nad"
+name             "nad-cookbook"
 supports         "ubuntu", ">= 10.0.0"    
 supports         "centos", ">= 5.0"    
 supports         "rhel",   ">= 5.0"    
+supports         "smartos", ">=0"
 
 depends         "smf"
 
