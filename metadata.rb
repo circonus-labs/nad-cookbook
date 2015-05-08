2aaeece8d20d92bade0f3ac8dbdd5b1dfaf2a49a
Philip Yuengling <pyuengling@client-10-8-3-83.office.omniti.com>
2015-05-08 17:24:37 -0400

    sane version bump

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "sa@omniti.com"
 license          "Modified BSD"
 description      "nad (nodejs monitoring agent)"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.0"
+version          "0.0.6"
 name             "nad-cookbook"
 supports         "ubuntu", ">= 10.0.0"    
 supports         "centos", ">= 5.0"    
