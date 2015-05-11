a86973d4509950bf31514feaf37fd6bf36e64a01
Philip Yuengling <pyuengling@Philips-MacBook-Pro.local>
2015-05-11 10:49:00 -0400

    revert to name nad in metadata

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ license          "Modified BSD"
 description      "nad (nodejs monitoring agent)"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.6"
-name             "nad-cookbook"
+name             "nad"
 supports         "ubuntu", ">= 10.0.0"    
 supports         "centos", ">= 5.0"    
 supports         "rhel",   ">= 5.0"    
